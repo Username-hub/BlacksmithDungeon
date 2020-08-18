992244a08e300a9c3f3a8fd8b07faac9b9128fb7
Username <artur1shults@gmail.com>
2020-08-18 22:32:33 +0200

    Add rapier

---
+++ WeaponDirectory/WeaponScreenScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+namespace WeaponDirectory
+{
+    public class WeaponScreenScript : MonoBehaviour
+    {
+
+        public GameObject WeaponChosePrefab;
+        
+        public int StartAttack()
+        {
+            int result = 0;
+
+            return result;
+        }
+    }
+}
