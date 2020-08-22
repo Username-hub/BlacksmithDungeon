d502b7aba7c2037e39c3c3fe6a65e8051269d1df
Username <artur1shults@gmail.com>
2020-08-22 14:17:23 +0200

    Rapier aim script

--- RapierMinigame/RapierAimScript.cs
+++ RapierMinigame/RapierAimScript.cs
@@ -1,18 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class RapierAimScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [HideInInspector]
+    public RapierMinigameScript rapierMinigameScript;
+
+    public float maxX = 500;
+    public float maxY = 180;
+
+    private void Start()
     {
-        
+        SetNewPosition();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetNewPosition()
     {
-        
+        Vector2 position = transform.position;
+        position.x = Random.Range(-maxX, maxX);
+        position.y = Random.Range(-maxY, maxY);
+        transform.position = position;
+    }
+
+    private void AimHit()
+    {
+        rapierMinigameScript.AimHit();
+        SetNewPosition();
+    }
+    private void Update()
+    {
+        if (Input.touchCount >= 1)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(ray.origin, ray.direction, out raycastHit))
+            {
+                if (raycastHit.collider.gameObject == this.gameObject)
+                {
+                    
+                }
+            }
+        }
     }
 }
